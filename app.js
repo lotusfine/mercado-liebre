cdaddc1bbdcd216b6602431856aa39216bae625e
lotusfine <lucasyalet94@gmail.com>
2021-11-01 23:05:24 -0300

    nueva actualizacion

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const app = express();
+const path = require ("path")
+const publicPatch = path.resolve(__dirname, "./public");
+app.use( express.static(publicPatch) );
 
 app.listen(3000, () => {
     console.log('Servidor corriendo en el puerto 3000');
 });
 
-app.get('/', function(req, res){
-    res.send('servidor funcionando')
-    });
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/views/home.html"));
+})
 
-    //hola mundo uno dos tres cutro
+
+
+
+
+
+/*app.get ('/', (req, res) => {
+    let htmlPath = path.resolve(__dirname, './views/');
+    res.sendFile (htmlPath);
+});*/
+
+/*app.get('/', function(req, res){
+    res.send('servidor funcionando')
+    });*/
